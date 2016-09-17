29ac1c1fb2f6db8f650812d3bbd91b4805859374
9Lukas5 <9Lukas5@users.noreply.github.com>
2016-09-17 08:14:14 +0200

    m7: GPU: replace spaces with tab-stops to keep it consistent

--- arch/arm/mach-msm/htc/m7/board-m7-gpu.c
+++ arch/arm/mach-msm/htc/m7/board-m7-gpu.c
@@ -259,11 +259,11 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 #ifdef CONFIG_GPU_OVERCLOCK
-                {
-                        .gpu_freq = 545000000,
-                        .bus_freq = 4,
-                        .io_fraction = 0,
-                },
+		{
+			.gpu_freq = 545000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
 #else
 		{
 			.gpu_freq = 400000000,
